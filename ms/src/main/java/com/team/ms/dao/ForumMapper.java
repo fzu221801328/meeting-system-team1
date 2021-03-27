cd885dec42557e8dcce94ec2dcc47f60923f6b3e
fzu-zsp <934110724@qq.com>
2021-03-27 15:03:16 +0800

    dao 空壳

---
+++ ms/src/main/java/com/team/ms/dao/ForumMapper.java
@@ -0,0 +1,9 @@
+package com.team.ms.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+public interface ForumMapper {
+}
